f7e2a00081f46021f65a9f4abc1c11aeceb8f01b
Soham <bagalsoham1717@gmail.com>
2025-08-03 22:55:22 +0530

    feat: User data update

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import exp from "constants";
 import Profile from "../models/profile.model.js";
 
 import User from "../models/user.model.js";
@@ -61,3 +62,59 @@ export const login  = async (req,res) =>{
         
     }
 } ;
+
+export const uploadProfilePicture =async(req,res)=>{
+    const {token} = req.body;
+    try {
+        const user = await User.findOne({token:token});
+        if(!user){
+            return res.status(404).json({message:"User not found!"})
+        }
+        user.profilePicture = req.file.filename;
+        await user.save();
+        return res.json({message:"Profile picture updated "});
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
+
+export const updateUserProfile = async(req,res)=>{
+    try {
+        const {token, ...newUserData} = req.body;
+
+        const user =await User.findOne({token:token});
+        if(!user){
+            return res.status(404).json({message:"User not found "})
+        }
+        const {username,email} = newUserData;
+
+        const existingUser = await user.findOne({$or: [{username},{email}]});
+
+        if(existingUser || String (existingUser._id) !== String(user._id)){//alow if the user is check if the existing user is not the user already here 
+            return res.status(400).json({message:"User already exists"})
+        }
+        Object.assign(user,newUserData);   
+        await user.save();
+        return res.json({message:"User got updated!"}) 
+
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
+
+export const getUserAndProfile = async(req,res)=>{
+    try {
+        const {token} = req.body;
+        const user = await User.findOne({token:token});
+
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+        const userProfile =await Profile.findOne({ userId:user._id})
+        .populate('userId','name email username profilePicture');
+
+        return res.json(userProfile)
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
-import { login, register } from "../controllers/user.controller.js";
+import { login, register, uploadProfilePicture,updateUserProfile,getUserAndProfile } from "../controllers/user.controller.js";
+import multer from 'multer';
 
 const router = Router();
 
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'profile_picture/')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
+    }
+})
+
+const upload = multer({storage:storage})
+router.route("/update_profile_picture")
+.post(upload.single('profile_picture'),uploadProfilePicture)
 
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route("/user_update").post(updateUserProfile)
+router.route("/get_user_and_profile").get(getUserAndProfile)
+
+
 export default router;
